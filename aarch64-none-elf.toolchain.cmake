edebd54378886178115b2b0a9514f2315f6c2be5
willintelligent <will.zhai.cn@gmail.com>
2025-08-30 16:42:32 +0800

    default cross-compile FreeRTOS kernel to aarch64 bare metal on Raspberry Pi 5 Cortex-A76

---
+++ aarch64-none-elf.toolchain.cmake
@@ -0,0 +1,9 @@
+set(CMAKE_SYSTEM_NAME Generic CACHE STRING "")
+set(CMAKE_SYSTEM_PROCESSOR aarch64 CACHE STRING "")
+
+set(TOOLCHAIN_PATH ${CMAKE_SOURCE_DIR}/aarch64-none-elf CACHE PATH "")
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PATH}/bin/aarch64-none-elf-gcc CACHE STRING "")
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PATH}/bin/aarch64-none-elf-g++ CACHE STRING "")
+
+set(CMAKE_C_FLAGS "-march=armv8-a -mtune=cortex-a76 -nostdlib -ffreestanding -Wall -Wextra" CACHE STRING "C flags")
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -fno-exceptions -fno-rtti" CACHE STRING "C++ flags") 
